a5e8be30ac0d834d12509c550129850e7fb16177
huzaifa56jbh <hadrish153@gmail.com>
2024-10-02 06:32:39 +0500

    d

--- app-trading-main/utils/allowedOrigins.js
+++ app-trading-main/utils/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
   "http://127.0.0.1:3500",
   "https://trading-app-client.vercel.app",
   "https://server-trading-app-production.up.railway.app",
-  "https://synthonext-frontend.vercel.app"
+  "https://synthonext-frontend.vercel.app",
+  "https://frontend-two-phi-11.vercel.app"
 ];
 
 module.exports = allowedOrigins;
